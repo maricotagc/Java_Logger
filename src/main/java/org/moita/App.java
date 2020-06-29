ef331f8ff6d8b100e318fb893ea8dfb270657aa5
maricotagc <36520845+maricotagc@users.noreply.github.com>
2020-06-29 23:34:55 +0200

    logger example

---
+++ src/main/java/org/moita/App.java
@@ -0,0 +1,36 @@
+package org.moita;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App {
+
+    private final static Logger LOGGER = LogManager.getLogger(App.class.getName());
+    private int result = 0;
+
+    public int div(int num1, int num2) throws Exception {
+        LOGGER.trace("Starting division");
+        LOGGER.info("Numbers being divided: " + num1 + " and " + num2);
+
+        if (num2 == 0) {
+            LOGGER.error("It is not possible to divide any number by zero");
+            throw new Exception("ERROR MSG");
+
+        } else {
+            result = num1 / num2;
+            LOGGER.trace("Division done");
+            LOGGER.info("Result is: " + result);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        App test = new App();
+        try {
+            test.div(10,0);
+//            test.div(10,5);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
